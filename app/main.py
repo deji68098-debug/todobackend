3624c5c5ec38e694ab5c2af919f7646c09c248fe
Oladeji <deji68098@gmail.com>
2026-05-07 12:20:52 +0100

    fix CORS for new frontend domain

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "http://localhost:3000",
-                   "https://todolist-psi-one-45.vercel.app"],
+                   "https://todofrontend-mocha.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
