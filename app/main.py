f371f872a7ecae789c17b7cc08585b8027d2ad5d
Oladeji <deji68098@gmail.com>
2026-05-07 09:59:02 +0100

    add production frontend to CORS

--- app/main.py
+++ app/main.py
@@ -9,7 +9,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000",
+                   "https://todolist-psi-one-45.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
